ff4b0614137359864bd60c9316f94b2e285e0f8e
Tony Trinh <tony19@gmail.com>
2018-09-08 23:22:32 -0500

    0.8.1-2 dist

---
+++ dist/matchers/toIncludeRepeated/predicate.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+exports.default = (string, substring, occurrences) =>
+  (string.match(new RegExp(substring, 'g')) || []).length === occurrences;
